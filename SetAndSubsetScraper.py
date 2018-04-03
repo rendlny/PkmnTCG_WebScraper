1b44945f40e33145f78bfe50b6f68c6361045a1e
Lauren Delaney <ren.delaney@gmail.com>
2018-04-03 12:35:44 +0100

    finds out if new sets are existing or not

--- SetAndSubsetScraper.py
+++ SetAndSubsetScraper.py
@@ -2,57 +2,37 @@ import urllib.request
 #import BeautifulSoup
 from bs4 import BeautifulSoup
 
-# get lastSet and lastSubset numbers from DB_check
+# get lastSet and lastSubset numbers & setCode from DB_check
 from DB_Check import *
 lastSet
 lastSubset
-
+setCode
+newSubset = lastSubset + 1
 ################################################################################
 #Function to return html details grabbed from given url
 ################################################################################
 setExists = True
-subsetNum = 1
+urlStart = "https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/"
 
+print("Checking for new sets and subsets...")
 while(setExists == True):
 
-    #specify url
-    urlStart = "https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/"
-
-    setCode = "sm" # sm / xy / bw
-    urlEnd = "?" + str(subsetNum )+ "-" + setCode + "=on"
+    #specify url , this url is for the first card of the given set
+    urlEnd = str(setCode) + "-series/" + str(setCode) + str(newSubset) + "/1/"
     url = urlStart + urlEnd
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
-    print(url)
 
     def scrapeUrl(url):
         webpage = urllib.request.urlopen(url)
         page = BeautifulSoup(webpage, "html.parser")
         return page
 
-    webpage = urllib.request.urlopen(url)
+    try:
+        page = scrapeUrl(url)
+        print("Subset [" + str(newSubset) + "] exists!")
+        newSubset +=1
 
-    #Parse html in 'page' & store in BS format
-    page = BeautifulSoup(webpage, "html.parser")
-
-#####NOT FINDING, MUST FIND IN SUBSET 6 TO BE WORKING CORRECTLY
-    divFind = page.findAll('div', class_='content-block')
-    imgFound = divFind[1].find('img')
-    if imgFound is not None:
-        print(imgFound)
-    else:
-        print('FAILURE')
-    #manectric = divCheck.find('img')
-
-    ##checking if manectric card is displayed, if so then set does not exist
-    ## set beginging with manetric card is displayed when a non existing set is searched
-    if imgFound is not None:
-        print(manectric)
-        print('Nada!')
+    except Exception:
+        print("Subset [" + str(newSubset) + "] does not exist.")
         setExists = False
-
-    else:
-        print('SubSet Exists!')
-        print(subsetNum)
-        subsetNum = subsetNum + 1
-################################################################################
